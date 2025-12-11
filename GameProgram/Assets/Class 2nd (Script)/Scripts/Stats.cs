25cc6c2ab0c76188679e5b13de8a777c69679223
tgahn04 <tgahn0524@gmail.com>
2025-12-11 19:41:58 +0900

    (2025.12.11) 스크립트

---
+++ GameProgram/Assets/Class 2nd (Script)/Scripts/Stats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+
+public class stats : MonoBehaviour
+{
+    public int strength = 5;
+    public int wisdom = 1;
+    public int dexterity = 0;
+
+    void Start()
+    {
+        Debug.Log("strength : " + strength);
+        Debug.Log("wisdom : " + wisdom);
+        Debug.Log("dexterity : " + dexterity);
+    }
+
+  
+}
