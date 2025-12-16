a998721f1d7b93d354f70a3edb3813c88a587274
tgahn04 <tgahn0524@gmail.com>
2025-12-16 19:45:38 +0900

    (2025.12.16) 유저 인터페이스

---
+++ GameProgram/Assets/Class4th (Component)/Scripts/Truck.cs
@@ -0,0 +1,28 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class Truck : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] GameObject[] wheels;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        for(int i = 0; i > wheels.Length; i++)
+        {
+            Debug.Log (wheels[i].name);
+        }
+
+        rigidbody.useGravity = false;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ GameProgram/Assets/Class5th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExecuteStart()
+    {
+        Debug.Log("Start");
+    }
+    public void ExecuteOption()
+    {
+        Debug.Log("Option");
+    }
+    public void ExecuteExit()
+    {
+        Debug.Log("Exit");
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
