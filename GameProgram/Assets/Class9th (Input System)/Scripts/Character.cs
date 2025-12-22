7fd668e11aeaefff1ad52fa5a1bba6acc60da11d
tgahn04 <tgahn0524@gmail.com>
2025-12-22 19:45:51 +0900

    (2025.12.22) 입력 시스템

---
+++ GameProgram/Assets/Class9th (Input System)/Scripts/Character.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] Vector3 direction;
+    [SerializeField] float speed;
+
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        transform.position = direction * speed * Time.deltaTime;
+
+        Debug.Log(direction);
+    }
+}
