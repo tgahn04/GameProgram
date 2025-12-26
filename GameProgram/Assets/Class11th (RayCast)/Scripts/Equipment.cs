5869d343fdb12a3202feb1319a28edf91bf7fe9a
tgahn04 <tgahn0524@gmail.com>
2025-12-26 19:46:00 +0900

    (2025.12.26) 광선 검출

---
+++ GameProgram/Assets/Class11th (RayCast)/Scripts/Equipment.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public abstract class Equipment : MonoBehaviour
+{
+    public abstract void equipment();
+
+}
---
+++ GameProgram/Assets/Class11th (RayCast)/Scripts/Mouse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+    [SerializeField] float duration = 1.0f;
+    [SerializeField] RaycastHit raycastHit;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+            {
+                Debug.DrawLine(ray.origin, raycastHit.point, Color.green, duration);
+            }
+        }
+    }
+    
+}
