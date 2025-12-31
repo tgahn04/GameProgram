bfb6c7f72dac8ec1bc78eb9ed4cba36db2de6cc1
tgahn04 <tgahn0524@gmail.com>
2025-12-31 19:44:32 +0900

    (2025.12.31) 조명 시스템

--- GameProgram/Assets/Class12th (Coroutine)/Scripts/CreateManager.cs
+++ GameProgram/Assets/Class12th (Coroutine)/Scripts/CreateManager.cs
@@ -1,24 +1,60 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VFX;
 
 public class CreateManager : MonoBehaviour
 {
     [SerializeField] GameObject prefab;
+    GameObject clone;
+    WaitForSeconds wait;
+    // [SerializeField] float timer = 0f;
 
-    private float timer = 0f;
+    // void Update()
+    // {
+    //     timer += Time.deltaTime;
+    // 
+    //     if (timer > 5f)
+    //     {
+    //         clone = Instantiate(prefab);
+    // 
+    //         clone.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+    // 
+    //         timer = 0f;
+    //     }
+    // }
 
-    void Update()
+    void Start()
     {
-        timer += Time.deltaTime;
+        StartCoroutine(Coroutine());
+    }
 
-        if (timer > 5f)
+    void Awake()
+    {
+        wait = new WaitForSeconds(5f);
+    }
+
+    IEnumerator Coroutine()
+    {
+
+        while (true)
         {
-            GameObject clone;
-
-            clone = Instantiate(prefab);
+            clone = Instantiate(prefab, gameObject.transform);
 
             clone.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
 
-            timer = 0f;
+            Random.Range(0, 3);
+
+            yield return wait;
         }
     }
+    
 }
+
+// public interface IEnumator
+// {
+//     object Current { get; } 현재 위치의 데이터
+//  
+//     bool MoveNext() 다음위치로 이동, 데이터가 있으면 true 없으면 false
+//     
+//     void Reset() index 위치를 초기화        
+// }
