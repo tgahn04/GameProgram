21c9ba2c624556539e2ceb136888de7f86118057
tgahn04 <tgahn0524@gmail.com>
2026-01-05 19:45:14 +0900

    (2025.1.5) 렌더링 파이프 라인

--- GameProgram/Assets/Class13th (Sound)/Scripts/Unit.cs
+++ GameProgram/Assets/Class13th (Sound)/Scripts/Unit.cs
@@ -1,14 +1,59 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
+
+public enum AnimationState
+{
+    IDLE,
+    MOVE,
+    ATTACK,
+    DIE
+}
 
 public class Unit : MonoBehaviour
 {
-    [SerializeField] AudioClip audioClip;
+    [SerializeField] Animator animator;
     [SerializeField] AudioSource audioSource;
+    [SerializeField] int index = 0;
+    [SerializeField] AnimationState state;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (animator.IsInTransition(0))
+        {
+            Debug.Log("Transtion");
+        }
+    }
+
+    public void Transtion(int count)
+    {
+        if(count == 0)
+        {
+            state = (AnimationState)((int)state % 3);
+
+            return;
+        }
+
+        index += count;
+
+        if (index % 3 == 0)
+        {
+            state++;
+        }
+    }    
+
+    
 
     public void Emit()
     {
-        audioSource.clip = Resources.Load<AudioClip>("Attack");
+        Debug.Log(state.ToString());
+
+        audioSource.clip = Resources.Load<AudioClip>(state.ToString());
 
         audioSource.Play();
     }
